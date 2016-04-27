ae33da327f524f6a56ea6ed69456a8959b05a6a7
Stefan Ewers <Stefan Ewers>
2016-04-27 17:45:16 +0200

    Why is this no standard function?

---
+++ Get-SID.ps1
@@ -0,0 +1,33 @@
+﻿function Get-SID
+{
+  <#
+      .SYNOPSIS
+      Returns the SID for a specified User
+      .DESCRIPTION
+      Returns the SID for a specified User
+      .EXAMPLE
+      Get-SID -UserName JohnDoe
+  #>
+  [CmdletBinding()]
+  param
+  (
+    [String]
+    [Parameter(Mandatory=$true)]
+    $UserName
+  )
+  
+  try
+  {
+    $objUser = New-Object System.Security.Principal.NTAccount($UserName)
+    $SID = $objUser.Translate([System.Security.Principal.SecurityIdentifier])
+    $SID = $SID.Value
+    return $SID
+  }
+  catch
+  {
+    "Error was $_"
+    $line = $_.InvocationInfo.ScriptLineNumber
+    "Error was in Line $line"
+  }
+}
+
