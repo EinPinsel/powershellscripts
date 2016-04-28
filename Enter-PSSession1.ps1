e186ba0afbda1cffd8c238d3bb9e2e1291b35e87
Stefan Ewers <Stefan Ewers>
2016-04-28 19:15:54 +0200

    Lab controlled hyper-v without a domain needs some special psremoting-stuff, right?

---
+++ Enter-PSSession1.ps1
@@ -0,0 +1,23 @@
+﻿<#
+    .Synopsis
+    Short description
+    .DESCRIPTION
+    Long description
+    .EXAMPLE
+    Example of how to use this cmdlet
+    .EXAMPLE
+    Another example of how to use this cmdlet
+#>
+function Enter-PSSession1
+{
+    [CmdletBinding()]
+    Param
+    (
+        # Param help description
+        [string]
+        $RemoteComputer
+    )
+
+    $Credential = Get-Credential
+    Enter-PSSession -ComputerName $RemoteComputer -Credential $Credential
+}
