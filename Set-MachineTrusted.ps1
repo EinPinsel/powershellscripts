3eb4c1d503e0f899b5aa77da3f31be96173c4ec7
Stefan Ewers <Stefan Ewers>
2016-04-28 19:16:24 +0200

    Execute on the guest to enable a specified machine for psremoting

---
+++ Set-MachineTrusted.ps1
@@ -0,0 +1,30 @@
+﻿function Set-MachineTrusted
+{
+  <#
+      .SYNOPSIS
+      Adds the specified Host into the TrustedHosts for WSMan
+      .DESCRIPTION
+      To enable PSRemoting quick and dirty,
+      you can add the host into the TrustedHosts 
+  #>
+  [CmdletBinding()]
+  param
+  (
+    [String]
+    [Parameter(Mandatory=$true)]
+    $TrustedHost
+  )
+  
+  Write-Verbose "The specified Host is $TrustedHost"
+  try
+  {
+    Set-Item -Path WSMan:\localhost\Client\TrustedHosts -Value $HyperVHost -Concatenate -Force -ErrorAction Stop
+    Write-Verbose "The $TrustedHost got added to TrustedHosts"
+  }
+  catch
+  {
+    "Error was $_"
+    $line = $_.InvocationInfo.ScriptLineNumber
+    "Error was in Line $line"
+  }
+}
