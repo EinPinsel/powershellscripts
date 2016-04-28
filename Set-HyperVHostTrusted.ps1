40687b6aab504699223bca3b8f342f3b0ba60c6e
Stefan Ewers <Stefan Ewers>
2016-04-28 19:16:59 +0200

    Add the Hyper-V Host to the TrustedHosts list so you can manage your VMs from your host without a domain :)

---
+++ Set-HyperVHostTrusted.ps1
@@ -0,0 +1,13 @@
+﻿function Set-HyperVHostTrusted
+{
+  <#
+      .SYNOPSIS
+      Retrieves the Hyper-V Host and adds it to the TrustedHosts for WSMan
+      .DESCRIPTION
+      The Hyper-V Host will be stored in the registry. To enable PSRemoting quick and dirty,
+      you can add the host into the TrustedHosts 
+  #>
+  $HyperVHost = (get-item 'HKLM:\SOFTWARE\Microsoft\Virtual Machine\Guest\Parameters').GetValue('HostName')
+  Set-Item -Path WSMan:\localhost\Client\TrustedHosts -Value $HyperVHost -Concatenate -Force
+}
+
